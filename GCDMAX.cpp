d42509b3c4a29cc47b66cc276de223e68b3fc3cc
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-09-08 01:18:06 -0700

    C++

---
+++ GCDMAX.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  freopen("GCDMAX.inp", "r", stdin);
+  freopen("GCDMAX.out", "w", stdout);
+  long long n, t;
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    cout<<n/2<<'\n';
+  }
+  return 0;
+}
