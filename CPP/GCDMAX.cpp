f2a173e157a8db399c2da7d7db2fde0665728000
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-09-08 15:19:10 +0700

    C++

--- CPP/GCDMAX.cpp
+++ CPP/GCDMAX.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main() {
   freopen("GCDMAX.inp", "r", stdin);
   freopen("GCDMAX.out", "w", stdout);
@@ -9,14 +7,7 @@ int main() {
   cin >> t;
   while (t--) {
     cin >> n;
-    long long ucln1 = 0;
-    for (int i = 1; i <= n; i++)
-      for (int j = i + 1; j <= n; j++) {
-        long long ucln = __gcd(i, j);
-        if (ucln > ucln1)
-          ucln1 = ucln;
-      }
-    cout << ucln1 << "\n";
+    cout<<n/2<<'\n';
   }
   return 0;
 }
