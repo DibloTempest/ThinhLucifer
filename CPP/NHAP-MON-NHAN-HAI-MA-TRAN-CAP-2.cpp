28d36037b3a40c325c7ab00ac964e580e15a8fdb
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-10-11 07:08:34 -0700

    Add files via upload

---
+++ CPP/NHAP-MON-NHAN-HAI-MA-TRAN-CAP-2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a,b,c,d,x,y,z,t;
+int main()
+{
+    cin>>a>>b>>c>>d;
+    cin>>x>>y>>z>>t;
+    cout<<a*x+b*z<<' '<< a*y+b*t<<endl<<c*x+d*z<<' '<< c*y+d*t;
+
+}
