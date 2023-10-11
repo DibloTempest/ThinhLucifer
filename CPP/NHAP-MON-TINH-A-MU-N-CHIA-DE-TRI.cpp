92e308860fcd1d23083ca9eeb8d28e93cff4aac1
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-10-11 21:30:07 +0700

    C++

--- CPP/NHAP-MON-TINH-A-MU-N-CHIA-DE-TRI.cpp
+++ CPP/NHAP-MON-TINH-A-MU-N-CHIA-DE-TRI.cpp
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mod 1000000007
-ll nhanando(ll a, ll b, ll c)
+long long a, n,  mod = 1e9 + 7;
+long long pw(long long a, long long n)
 {
-    if (b == 0)
-        return 0;
-    ll tam = nhanando(a, b / 2, c);
-    tam = (tam + tam) % c;
-    if (b % 2 == 1)
-        tam = (tam + a) % c;
-    return tam;
-}
-ll pw(ll a, ll n)
-{
-    if (n == 0)
+    if(n == 0)
         return 1;
-    ll tam = pw(a, n / 2);
-    tam = (tam * tam) % mod;
-    if (n % 2 == 1)
-        tam = tam * a % mod;
-    return tam;
+    long long  tp = pw(a, n / 2);
+    tmp = (tp * tp) % mod;
+    if(n % 2 == 1)
+        tp = tp * a % mod;
+    return tp;
 }
-main()
+
+int main()
+
 {
-    ll a, n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> a >> n;
-    cout << pw(a, n);
+    cout << pw(a, n) % mod;
 }
