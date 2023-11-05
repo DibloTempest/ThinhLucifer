599920b7daeb8f0c5d7457c663315a269712d975
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-11-05 21:39:12 +0700

    C++

--- CPP/BS26-TIM-KIEM-NHI-PHAN-26-DAY-SO.cpp
+++ CPP/BS26-TIM-KIEM-NHI-PHAN-26-DAY-SO.cpp
@@ -1,22 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n;
+#define ll long long
+ll n, s = 0;
+ll k;
 int main()
+
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    cin >> n;
-    long long dau=1,cuoi= 2e9,kq;
-    while(dau<=cuoi)
-    {
-    long long giua=(dau+cuoi)/2;
-        if(giua*(giua+1)/2==n)
-        {
-            kq=giua;
-            cuoi=giua-1;
-        }
-        else dau=giua+1;
-    }
-    cout << kq;
+    cin>>n;
+    ll s = (sqrt(8 * n - 7) + 1) / 2;
+    cout << s;
 }
