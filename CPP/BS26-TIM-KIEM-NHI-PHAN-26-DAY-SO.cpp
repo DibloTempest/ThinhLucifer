8cf171a8a33a1ad42ae86d53cb4345820bc2d439
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-11-05 21:30:36 +0700

    C++

--- CPP/BS26-TIM-KIEM-NHI-PHAN-26-DAY-SO.cpp
+++ CPP/BS26-TIM-KIEM-NHI-PHAN-26-DAY-SO.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 long long n;
-long long check(long long m)
-{
-    long long d = 0, cnt = 1;
-    return (1 + m) * m / 2;
-}
-long long bs()
-{
-    long long l = 1, r = 100000000, res;
-    while (l <= r)
-    {
-        long long m = (l + r) / 2;
-        if (check(m) >= n)
-        {
-            r = m - 1;
-            res = m;
-        }
-        else
-            l = m + 1;
-    }
-    return res;
-}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin >> n;
-    cout << bs();
+    long long dau=1,cuoi= 2e9,kq;
+    while(dau<=cuoi)
+    {
+    long long giua=(dau+cuoi)/2;
+        if(giua*(giua+1)/2==n)
+        {
+            kq=giua;
+            cuoi=giua-1;
+        }
+        else dau=giua+1;
+    }
+    cout << kq;
 }
