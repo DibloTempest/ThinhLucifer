39af1c4b388bacb8caa852c6ad9a39ac6fb03aeb
Rimuru <138693469+DibloTempest@users.noreply.github.com>
2023-11-26 07:09:39 -0800

    C++

--- CPP/SO-DOI-XUNG-MUC-1.cpp
+++ CPP/SO-DOI-XUNG-MUC-1.cpp
@@ -7,8 +7,8 @@ ll dem(ll x) {
     if (x < 10) return x;
     ll kq = 9 + (x / 10);
     ll s = x;
-    while (s > 10) s = s / 10;
-    if (s == (x % 10)) kq--;
+    while (s >= 10) s = s / 10;
+    if (s >= (x % 10)) kq--;
     return kq;
 }
 int main() {
